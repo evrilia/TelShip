fbd55216268aa721bb8494c5f130c5f7feb80d92
evrilia <evriliaelsyabilaa@gmail.com>
2026-02-13 09:21:58 +0700

    fix footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer bg-navy text-white p-5 mt-5">
     <div class="container-fluid px-0">
-        <h4 class="fw-bold mb-2">Witel Purwokerto</h4>
+        <h4 class="fw-bold mb-2">Telkom Daerah Purwokerto</h4>
         <p class="opacity-75 mb-4">
             Jl. Gerilya Barat Tj. No.272–274, Tanjung, Bojong, <br>
             Kec. Purwokerto Selatan, Kabupaten Banyumas, <br>
